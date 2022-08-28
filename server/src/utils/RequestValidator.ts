912b937ab5e2d92d90836bc715f7419af9a6c532
MarcHamamji <hamamjimarc@gmail.com>
2022-08-28 19:00:44 +0300

    Fix RequestValidator

--- server/src/utils/RequestValidator.ts
+++ server/src/utils/RequestValidator.ts
@@ -1,10 +1,10 @@
-import { Request, Response, NextFunction} from 'express';
-import { AnyZodObject } from 'zod';
+import { Request, Response, NextFunction } from 'express';
+import zod from 'zod';
 
 interface ValidationObject {
-  body?: AnyZodObject;
-  params?: AnyZodObject;
-  query?: AnyZodObject;
+  body?: zod.AnyZodObject;
+  params?: zod.AnyZodObject;
+  query?: zod.AnyZodObject;
 }
 
 export default function requestValidator(validationObject: ValidationObject) {
@@ -12,12 +12,14 @@ export default function requestValidator(validationObject: ValidationObject) {
     let key: keyof ValidationObject;
     for (key in validationObject) {
       const validator = validationObject[key];
-      if(!validator) continue;
+      if (!validator) continue;
       try {
         req[key] = await validator.parseAsync(req[key]);
       } catch (error) {
-        res.status(400);
-        next(error);
+        if (error instanceof zod.ZodError) {
+          res.status(400);
+          next(new Error(error.issues.map(i => i.message).join(' - ')))
+        }
       }
     }
     next();
