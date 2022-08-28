cc3a4c0e0ad477a93124ce85c3996d5c796b5965
MarcHamamji <hamamjimarc@gmail.com>
2022-08-28 17:28:54 +0300

    Don't allow empty environment variables

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -5,16 +5,16 @@ dotenv.config();
 
 const schema = zod.object({
   NODE_ENV: zod.enum(['development', 'production']).default('development'),
-  HOST: zod.string(),
-  SERVER_PORT: zod.string().regex(/[0-9]+/),
-  CLIENT_URL: zod.string().url(),
-  JWT_SECRET: zod.string(),
-  GITHUB_OAUTH_CLIENT_ID: zod.string(),
-  GITHUB_OAUTH_CLIENT_SECRET: zod.string(),
-  POSTGRES_HOST: zod.string(),
-  POSTGRES_USER: zod.string(),
-  POSTGRES_PASSWORD: zod.string(),
-  POSTGRES_DB: zod.string(),
+  HOST: zod.string().min(1),
+  SERVER_PORT: zod.string().regex(/[0-9]+/).min(2),
+  CLIENT_URL: zod.string().url().min(1),
+  JWT_SECRET: zod.string().min(1),
+  GITHUB_OAUTH_CLIENT_ID: zod.string().min(1),
+  GITHUB_OAUTH_CLIENT_SECRET: zod.string().min(1),
+  POSTGRES_HOST: zod.string().min(1),
+  POSTGRES_USER: zod.string().min(1),
+  POSTGRES_PASSWORD: zod.string().min(1),
+  POSTGRES_DB: zod.string().min(1),
 });
 
 const config = schema.parse(process.env);
