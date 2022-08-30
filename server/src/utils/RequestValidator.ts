f6108f1e69be5e2829fb0c43ce2ac61380d728bb
MarcHamamji <hamamjimarc@gmail.com>
2022-08-30 18:04:04 +0300

    Fix RequestValidator

--- server/src/utils/RequestValidator.ts
+++ server/src/utils/RequestValidator.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-continue */
 import { Request, Response, NextFunction } from 'express';
 import zod from 'zod';
 
@@ -10,15 +11,17 @@ interface ValidationObject {
 export default function requestValidator(validationObject: ValidationObject) {
   return async (req: Request, res: Response, next: NextFunction) => {
     let key: keyof ValidationObject;
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
     for (key in validationObject) {
       const validator = validationObject[key];
       if (!validator) continue;
       try {
+        // eslint-disable-next-line no-await-in-loop
         req[key] = (await validator.parseAsync(req[key])) as zod.infer<typeof validator>;
       } catch (error) {
         if (error instanceof zod.ZodError) {
           res.status(400);
-          next(new Error(error.issues.map(i => i.message).join(' - ')))
+          next(new Error(error.issues.map((i) => i.message).join(' - ')));
         }
       }
     }
