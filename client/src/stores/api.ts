1a500b9ec768b9f4ecab15f0bc8a2eff8b5e6ee7
MarcHamamji <hamamjimarc@gmail.com>
2022-10-10 14:23:29 +0300

    Use Vue 3 syntax to define store

--- client/src/stores/api.ts
+++ client/src/stores/api.ts
@@ -1,27 +1,31 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
-const useAPI = defineStore({
-  id: 'api',
-  state: () => ({
-    serverURL: import.meta.env.VITE_SERVER_URL,
-  }),
-  actions: {
-    fetch(input: RequestInfo | URL, init?: RequestInit | undefined) {
-      return fetch(input, {
-        ...init,
-        headers: {
-          authorization: window.localStorage.getItem('jwt') || '',
-          ...init?.headers,
-        },
-      });
-    },
-    async isLoggedIn() {
-      const response = await this.fetch(`${this.serverURL}/api/v1/ping`, {
-        method: 'GET',
-      });
-      return response.status === 200;
-    },
-  },
+const useAPI = defineStore('api', () => {
+  const serverURL = ref(import.meta.env.VITE_SERVER_URL);
+
+  function customFetch(input: RequestInfo | URL, init?: RequestInit | undefined) {
+    return fetch(input, {
+      ...init,
+      headers: {
+        authorization: window.localStorage.getItem('jwt') || '',
+        ...init?.headers,
+      },
+    });
+  }
+
+  async function isLoggedIn() {
+    const response = await fetch(`${serverURL.value}/api/v1/ping`, {
+      method: 'GET',
+    });
+    return response.status === 200;
+  }
+
+  return {
+    serverURL,
+    fetch: customFetch,
+    isLoggedIn,
+  };
 });
 
 export default useAPI;
