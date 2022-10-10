2f053dbc8562682ccf94d4722c968b9bf3252447
MarcHamamji <hamamjimarc@gmail.com>
2022-10-10 14:17:10 +0300

    Use Vue 3 syntax to define store

--- client/src/stores/stories.ts
+++ client/src/stores/stories.ts
@@ -1,33 +1,37 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 import type Story from '@/types/Story';
 import useAPI from './api';
 
-const useStories = defineStore({
-  id: 'stories',
-  state: () => ({
-    stories: [] as Story[],
-  }),
-  actions: {
-    async refreshStories() {
-      const api = useAPI();
-      const response = await api.fetch(`${api.serverURL}/api/v1/stories`, {
-        method: 'GET',
-      });
-      const data = await response.json();
-      this.stories = data.stories;
-    },
-    async getStory(storyId: string) {
-      const api = useAPI();
-      const response = await api.fetch(`${api.serverURL}/api/v1/stories/${storyId}`, {
-        method: 'GET',
-      });
-      if (response.status !== 200) {
-        window.location.href = '/';
-      }
-      const data = await response.json();
-      return data;
-    },
-  },
+const useStories = defineStore('stories', () => {
+  const stories = ref<Story[]>([]);
+
+  async function refreshStories() {
+    const api = useAPI();
+    const response = await api.fetch(`${api.serverURL}/api/v1/stories`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    stories.value = data.stories;
+  }
+
+  async function getStory(storyId: string) {
+    const api = useAPI();
+    const response = await api.fetch(`${api.serverURL}/api/v1/stories/${storyId}`, {
+      method: 'GET',
+    });
+    if (response.status !== 200) {
+      window.location.href = '/';
+    }
+    const data = await response.json();
+    return data;
+  }
+
+  return {
+    stories,
+    refreshStories,
+    getStory,
+  };
 });
 
 export default useStories;
