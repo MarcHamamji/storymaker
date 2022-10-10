8d2e2204cfe008fd202acadd6bbd2dd9d3091f1c
MarcHamamji <hamamjimarc@gmail.com>
2022-10-10 14:13:34 +0300

    Use Vue 3 syntax to define store

--- client/src/stores/drawflow.ts
+++ client/src/stores/drawflow.ts
@@ -1,40 +1,46 @@
 import type Drawflow from 'drawflow';
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 import useAPI from './api';
 
-const useDrawflow = defineStore({
-  id: 'drawflow',
-  state: () => ({
-    editor: null as Drawflow | null,
-  }),
-  actions: {
-    setEditor(df: Drawflow) {
-      this.editor = df;
-    },
-    refreshConnections(nodeid: string) {
-      this.editor?.updateConnectionNodes(nodeid);
-    },
-    async saveFlow(storyId: string) {
-      if (!this.editor) {
-        throw new Error('Editor not initialized');
-      }
-      const api = useAPI();
-      const response = await api.fetch(`${api.serverURL}/api/v1/stories/${storyId}`, {
-        method: 'PATCH',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          flow: this.editor.export(),
-        }),
-      });
-      const json = await response.json();
+const useDrawflow = defineStore('drawflow', () => {
+  const editor = ref<Drawflow | null>(null);
 
-      if (json.message !== 'OK') {
-        throw new Error(json.message || 'Unable to save story');
-      }
-    },
-  },
+  function setEditor(df: Drawflow) {
+    editor.value = df;
+  }
+
+  function refreshConnections(nodeid: string) {
+    editor.value?.updateConnectionNodes(nodeid);
+  }
+
+  async function saveFlow(storyId: string) {
+    if (editor.value === null) {
+      throw new Error('Editor not initialized');
+    }
+    const api = useAPI();
+    const response = await api.fetch(`${api.serverURL}/api/v1/stories/${storyId}`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        flow: editor.value.export(),
+      }),
+    });
+    const json = await response.json();
+
+    if (json.message !== 'OK') {
+      throw new Error(json.message || 'Unable to save story');
+    }
+  }
+
+  return {
+    editor,
+    setEditor,
+    refreshConnections,
+    saveFlow,
+  };
 });
 
 export default useDrawflow;
